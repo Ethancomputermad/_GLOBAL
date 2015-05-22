e4ee7d9e761d75913c4cb7d0c2e9a3ab08a0bc38
Ethancomputermad <ethanmayersweet@me.com>
2015-05-22 06:06:12 +0100

    Create LocalScript.lua

---
+++ examples/LocalScript.lua
@@ -0,0 +1,7 @@
+repeat wait() until _G.devethan repeat wait() until _G.devethan.ini
+
+_G.devethan.ini('_GLOBAL') --This means load all DevEthan packages, but dont finish until you've definetally loaded the package _GLOBAL
+
+_GLOBAL.hi='Hello, world!'
+print(_GLOBAL.hi)
+
